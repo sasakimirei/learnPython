e2a7bf97d1ad17939d90be1de40a827bc04da592
mirei <tienvm272@thefirstone.jp>
2025-09-29 17:32:26 +0700

    end of day 19

--- web.py
+++ web.py
@@ -16,8 +16,13 @@ st.write("This app is to increase your productivity.")
 # st.checkbox("Buy grocery")
 # st.checkbox("Throw the trash")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="add", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
